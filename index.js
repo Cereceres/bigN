793d0898c4afb0afb94ff141b8c307874bd6650b
Jesús Edel Cereceres Delgado <jesus.edelcereceres@gmail.com>
2016-11-25 23:25:41 +0000

    the index doc is added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict'
 const regex = /[1-9]0+$/
 class bigN {
+  /**
+   * @class contructor
+   * @param {string} the number to be instances
+   * @param {number} the decimal to be used
+   *
+   */
   constructor (number, numDecimal) {
     number = number || 0
     let decimal = [ ]
@@ -12,37 +18,37 @@ class bigN {
   }
   /**
    * sum method
-   *
+   * @param {string} number to be added
    */
   add (number) {
-    console.log('number', number)
+    //split the number in decimal a integer part
     let num = number.toString().split('.')
-    console.log('num', num)
+    // set the decimal part
     let decimal = [ ]
+    // get the decimal part
     num[ 1 ] && (decimal = num[ 1 ].split(''))
+    // get the integer part
     num = num[ 0 ].split('')
+    // get the complete number
     num = num.concat(decimal)
+    // set the auxiliary number
     let a, b
+    // the complentary part
     let _res = 0,
       res = [ ]
-    console.log('this.decimal', this.decimal)
+    // the decimal numbers to be used  
     let numDecimal = decimal.length > this.decimal ? decimal.length : this.decimal
-    console.log('numDecimal', numDecimal)
+    // maz number of decimals
     let max = numDecimal + ((num.length - decimal.length) > (this.number.length - this.decimal)
     ? num.length - decimal.length : this.number.length - this.decimal)
-    console.log('max', max)
+    // numbers max to use
     let numMax = this.number.length - this.decimal + numDecimal
-    console.log('numMax', numMax)
+    // the second max numer
     let numMax2 = num.length - decimal.length + numDecimal
-    console.log('numMax2', numMax2)
-    console.log('this.number', this.number)
     for (let i = 1; i <= max; i++) {
       a = this.number[ numMax - i ] !== undefined ? +(this.number[ numMax - i ]) : 0
-      console.log('a', a)
       b = num[ numMax2 - i ] !== undefined ? +(num[ numMax2 - i ]) : 0
-      console.log('b', b)
       res[ max - i ] = a + b + _res
-      console.log('res', res)
       if (res[ max - i ] > 9) {
         _res = 1
         res[ max - i ] = res[ max - i ] % 10
@@ -50,8 +56,11 @@ class bigN {
         _res = 0
       }
     }
+    // get the decimal
     this.decimal = numDecimal
+    // get the number like array
     this.number = res
+    // set the zeros found them
     let zeros = this.number.join('').search(regex)
     zeros > -1 
     && (this.number = this.number.slice(0, zeros + 1)) 
@@ -63,6 +72,7 @@ class bigN {
   }
   /**
    * multiply method
+   * @param {string} number to be multplyed
    *
    */
   multiply (number) {
@@ -110,6 +120,7 @@ class bigN {
   }
   /**
    * rest method
+   * @param {string} param to ve rested
    *
    */
   rest (number) {
@@ -129,7 +140,6 @@ class bigN {
     for (var i = 1; i <= max; i++) {
       a = this.number[ numMax - i ] !== undefined ? +(this.number[ numMax - i ]) : 0
       b = num[ numMax2 - i ] !== undefined ? +(num[ numMax2 - i ]) : 0
-      console.log('a,b', a, b, _res)
       if (a >= b) {
         res[ max - i ] = a - b - _res
         _res = 0
@@ -137,7 +147,6 @@ class bigN {
         res[ max - i ] = a + 10 - b - _res
         _res = 1
       }
-      console.log('res',res)
     }
     this.decimal = numDecimal
     this.number = res
@@ -148,53 +157,6 @@ class bigN {
     zeros = this.number.reverse().join('').search(regex)
     this.number.reverse()
     zeros > -1 && (this.number = this.number.slice(this.number.length - zeros - 1)) 
-    return this
-  }
-  /**
-   * divition method
-   *
-   */
-  div (number, numDecimal) {
-    console.log('this.number', number, this.number, this.decimal)
-    let num = number.toString().split('.')
-    let decimal = [ ]
-    num[ 1 ] && (decimal = num[ 1 ].split(''))
-    num = num[ 0 ].split('')
-    num = num.concat(decimal)
-    let min, max
-    max = this.number.length
-    min = num.length
-    let dif = max - min
-    dif = dif < 0 ? 0 : dif
-    let res = [ ],
-      a, b, _res
-    numDecimal = numDecimal === undefined ? this.decimal : numDecimal
-    let n = numDecimal - this.decimal + decimal.length + dif
-    a = +(this.number.slice(0, dif == 0 ? max : min).join(''))
-    b = +(num.join(''))
-    for (var i = 0; i <= n; i++) {
-      res[ i ] = Math.floor(a / b)
-      _res = a - b * res[ i ]
-      a = +(_res.toString().concat(this.number[ i + min ] === undefined
-        ? 0 : this.number[ i + min ]))
-      if (!_res && this.number[ i + min ] === undefined) { break }
-    }
-    console.log('res', res)
-    this.number = res.slice(0,res.length-this.decimal + decimal.length + numDecimal)
-    this.decimal = numDecimal
-    console.log('this.number', this.number)
-    let zeros = this.number.join('').search(regex)
-    zeros > -1 
-    && (this.number = this.number.slice(0, zeros + 1)) 
-    && (this.decimal = this.decimal + zeros - this.number.length)
-    console.log('this.decimal', this.decimal, this.number)
-    if((this.number.length === this.decimal + 1) && this.number[0] === 0 ) return this
-    zeros = this.number.reverse().join('').search(regex)
-    console.log('zeros', zeros)
-    this.number.reverse()
-    zeros > -1 &&
-    (this.number = this.number.slice(this.number.length - zeros - 1)) 
-    console.log('this.number', this.number, this.decimal)
     return this
   }
   /**
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict'
 let assert = require('assert'),
   bigN = require('../index')
-describe('Test for routines ob bigN', function () {
+describe('Test for routines of bigN', function () {
   let num = new bigN('23.234')
   it('the sum test with', function () {
     assert.equal(num.add('1234.1224').toString(), '1257.3564')
@@ -14,15 +14,5 @@ describe('Test for routines ob bigN', function () {
   it('the multiply test with', function () {
     assert.equal(new bigN('1.6').multiply('1.2').toString(),
       '1.92')
-  })
-
-  it('the divition test with', function () {
-    assert.equal(new bigN('1.6').div('2').toString(), '0.8')
-  })
-
-  it('the divition test with', function () {
-    let n = num.div('5', 0).toString()
-    console.log('n=', n)
-    assert.equal(n, '23.233952')
   })
 })
